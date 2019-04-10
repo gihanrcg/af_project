8d46b4a2128bb3ee2b25575077c13b8c10e15260
test123 <nishithadesilva123@gmail.com>
2019-04-10 20:30:59 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import LeaderBoard from "./LeaderBoard";
 import SignUp from "./SignUp";
+import Home from "./Home";
 
 class App extends Component {
   render() {
-
+      if(false){
+         return( <div style={{display:"flex"}} >
+              <Home/>
+          </div>);
+      }else
     return (
       <div style={{display:"flex"}} >
 
